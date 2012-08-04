f84be65b8f8d58b9542df8d276dc4ce14b997158
z80 <litedictteam@gmail.com>
2012-08-04 20:46:01 +0400

    Added function prototypes.

--- software/libio/src/ctrlboard_io.cpp
+++ software/libio/src/ctrlboard_io.cpp
@@ -28,35 +28,238 @@ CtrlboardIo::~CtrlboardIo()
 {
 }
 
-int  CtrlboardIo::version( std::string & ver )
+bool CtrlboardIo::version( std::string & ver )
 {
     execFunc( FUNC_VERSION );
     std::basic_string<unsigned char> & from = dataFrom();
     int sz = read( from, 8 );
     if ( sz < 2 )
-        return -1;
+        return false;
     ver.resize( sz );
     char * d = reinterpret_cast<char *>( const_cast<unsigned char *>( from.data() ) );
     for ( int i=0; i<sz; i++ )
         ver[i] = d[i];
-    return sz;
+    return true;
 }
 
-int  CtrlboardIo::firmware( std::string & fir )
+bool CtrlboardIo::firmware( std::string & fir )
 {
     execFunc( FUNC_FIRMWARE );
     std::basic_string<unsigned char> & from = dataFrom();
     int sz = read( from, 8 );
     if ( sz < 2 )
-        return -1;
+        return false;
     fir.resize( sz );
     char * d = reinterpret_cast<char *>( const_cast<unsigned char *>( from.data() ) );
     for ( int i=0; i<sz; i++ )
         fir[i] = d[i];
-    return sz;
+    return true;
 }
 
+bool CtrlboardIo::setMotorControl( TMotorControl val )
+{
+    return true;
+}
 
+bool CtrlboardIo::motorControl( TMotorControl & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::setThrottleRumpUp( int val )
+{
+    return true;
+}
+
+bool CtrlboardIo::throttleRumpUp( int & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::setThrottleRumpDown( int val )
+{
+    return true;
+}
+
+bool CtrlboardIo::setThrottleRumpDown( int & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::setThrottleRangeLow( int val )
+{
+    return true;
+}
+
+bool CtrlboardIo::throttleRangeLow( int & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::setThrottleRangeHigh( int val )
+{
+    return true;
+}
+
+bool CtrlboardIo::throttleRangeHigh( int & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::setThrottleLockout( bool val )
+{
+    return true;
+}
+
+bool CtrlboardIo::throttleLockout( bool & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::setStallThreshold( int val )
+{
+    return true;
+}
+
+bool CtrlboardIo::stallThreshold( int & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::setThrottleSpeedCtrl( TSpeedCtrl val )
+{
+    return true;
+}
+
+bool CtrlboardIo::throttleSpeedCtrl( TSpeedCtrl & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::setCurrentLimit( int en )
+{
+    return true;
+}
+
+bool CtrlboardIo::throttleSpeedCtrl( int & en )
+{
+    return true;
+}
+
+bool CtrlboardIo::setUndervoltageCtrl( int en )
+{
+    return true;
+}
+
+bool CtrlboardIo::undervoltageCtrl( int & en )
+{
+    return true;
+}
+
+bool CtrlboardIo::setPassword( const std::string & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::password( std::string & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::setEnabled( bool en )
+{
+    return true;
+}
+
+bool CtrlboardIo::enabled( bool & en )
+{
+    return true;
+}
+
+bool CtrlboardIo::setThrottle( int val )
+{
+    return true;
+}
+
+bool CtrlboardIo::throttle( int & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::setSpeed( int val )
+{
+    return true;
+}
+
+bool CtrlboardIo::speed( int & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::setDirection( bool cw )
+{
+    return true;
+}
+
+bool CtrlboardIo::direction( bool & cw )
+{
+    return true;
+}
+
+bool CtrlboardIo::voltage( int & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::enKeySeat_1( int & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::enKeySeat_2( int & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::controllerT( int & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::motorT( int & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::extT_1( int & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::extT_2( int & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::errorCode( int & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::hours( int & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::cycles( int & val )
+{
+    return true;
+}
+
+bool CtrlboardIo::modelRev( int & val )
+{
+    return true;
+}
 
 
 
--- software/libio/src/ctrlboard_io.h
+++ software/libio/src/ctrlboard_io.h
@@ -9,14 +9,77 @@
 class CtrlboardIo: public LowlevelIo
 {
 public:
+    enum TMotorControl { TCurrentLoop=0, TAnalogLevel=1, TSoftware=2 };
+    enum TSpeedCtrl { TThrottle=0, TSpeed=1 };
     typedef std::basic_string< unsigned char > TIo;
 
     CtrlboardIo();
     ~CtrlboardIo();
 
-    int version( std::string & ver );
-    int firmware( std::string & fir );
+    bool version( std::string & ver );
+    bool firmware( std::string & fir );
 
+    // Group 1.
+    bool setMotorControl( TMotorControl val );
+    bool motorControl( TMotorControl & val );
+
+    bool setThrottleRumpUp( int val );
+    bool throttleRumpUp( int & val );
+
+    bool setThrottleRumpDown( int val );
+    bool setThrottleRumpDown( int & val );
+
+    bool setThrottleRangeLow( int val );
+    bool throttleRangeLow( int & val );
+
+    bool setThrottleRangeHigh( int val );
+    bool throttleRangeHigh( int & val );
+
+    bool setThrottleLockout( bool val );
+    bool throttleLockout( bool & val );
+
+    bool setStallThreshold( int val );
+    bool stallThreshold( int & val );
+
+    bool setThrottleSpeedCtrl( TSpeedCtrl val );
+    bool throttleSpeedCtrl( TSpeedCtrl & val );
+
+    bool setCurrentLimit( int en );
+    bool throttleSpeedCtrl( int & en );
+
+    bool setUndervoltageCtrl( int en );
+    bool undervoltageCtrl( int & en );
+
+    bool setPassword( const std::string & val );
+    bool password( std::string & val );
+
+    // Group 2.
+    bool setEnabled( bool en );
+    bool enabled( bool & en );
+
+    bool setThrottle( int val );
+    bool throttle( int & val );
+
+    bool setSpeed( int val );
+    bool speed( int & val );
+
+    bool setDirection( bool cw );
+    bool direction( bool & cw );
+
+    // Group 3.
+    bool voltage( int & val );
+    bool enKeySeat_1( int & val );
+    bool enKeySeat_2( int & val );
+    bool controllerT( int & val );
+    bool motorT( int & val );
+    bool extT_1( int & val );
+    bool extT_2( int & val );
+    bool errorCode( int & val );
+
+    // Group 3-eeprom.
+    bool hours( int & val );
+    bool cycles( int & val );
+    bool modelRev( int & val );
 };
 
 
