ce3c1454fb7cdcee300cced7294c101c8cdddeaa
z80 <litedictteam@gmail.com>
2012-08-04 09:37:54 +0400

    Signed-off-by: z80 <litedictteam@gmail.com>

---
+++ firmware/src/cpu_io.c
@@ -0,0 +1,19 @@
+
+#include "cpu_io.h"
+
+#define BUFFER_SZ 16
+
+unsigned char g_ioPtr = 0;
+uchar g_ioBuffer[ BUFFER_SZ ];
+
+
+void cpuIoInit( void )
+{
+}
+
+void cpuIoIo( uchar * in, uchar * out )
+{
+}
+
+
+
---
+++ firmware/src/cpu_io.h
@@ -0,0 +1,10 @@
+
+#ifndef __CPU_IO_H_
+#define __CPU_IO_H_
+
+void cpuIoInit( void );
+void cpuIoIo( uchar * in, uchar * out );
+
+#endif
+
+
