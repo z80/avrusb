15ddcec248e7457c66f4e9e049083530919cf55f
z80 <litedictteam@gmail.com>
2012-08-20 21:45:12 +0400

    Adjusted parameters set to be the same as in the latest task description.

--- common/config.h
+++ common/config.h
@@ -5,24 +5,33 @@
 #define VERSION "0.0.1"
 #define FIRMWARE "moto"
 
-// Param IDs.
-#define MOTOR_CONTROL       0
-#define THROTTLE_RUMP_UP    1
-#define THROTTLE_RUMP_DOWN  3
-#define THROTTLE_RANGE_LOW  5
-#define THROTTLE_RANGE_HIGH 6
-#define THROTTLE_LOCKOUT    7
-#define STALL_THRESHOLD     8
-#define THROTTLE_SPEED_CTRL 9
-#define CURRENT_LIMIT       10
-#define UNDERVOLTAGE_CTRL   12
-#define PASSWORD            14
+// Param IDs (in EEPROM).
+#define MOTOR_CONTROL       		0
+#define THROTTLE_SPEED_CTRL 		1
+#define MAX_THROTTLE_CW			2
+#define MAX_THROTTLE_CCW			3
+#define MAX_SPEED_CW 				4
+#define MAX_SPEED_CCW 			6
+#define THROTTLE_RUMP_UP_CW    	8
+#define THROTTLE_RUMP_UP_CCW		10
+#define THROTTLE_RUMP_DOWN_CW  	12
+#define THROTTLE_RUMP_DOWN_CCW	14
+#define COMMUTATION_MODE  		16
+#define THROTTLE_LOCKOUT    		17
+#define STALL_THRESHOLD     		18
+#define CURRENT_LIMIT       		20
+#define UNDERVOLTAGE_CTRL   		22
+#define MOTOR_OVERTEMP 			24
+#define CONTROLLER_OVERTEMP 		26
+#define PASSWORD            		28
 
+// Software ctrl params (in RAM).
 #define ON_OFF              0
 #define THROTTLE            1
 #define SPEED               2
 #define DIRECTION           4
 
+// Ram runtime parameters, probably, for state visual control...
 #define VOLTAGE             5
 #define ENABLE_KEY_SEAT_1   6
 #define ENABLE_KEY_SEAT_2   7
@@ -32,10 +41,12 @@
 #define EXT_T_2             14
 #define ERROR               16
 
-#define HOURS               30
-#define CYCLES              32
-#define MODEL_REV           34
+// Three additional parameters from EEPROM...
+#define HOURS               44
+#define CYCLES              46
+#define MODEL_REV           48
 
+// Functions which could be invoked in MCU.
 #define FUNC_VERSION          1 // Firmware version.
 #define FUNC_FIRMWARE         2 // Firmware description.
 #define FUNC_SET_PARAM        3 // Invokes param set routine.
